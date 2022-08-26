64e3996f98e8f74d0360cae00e242f6f61dda7ae
Learn Ner <mrlearnner@gmail.com>
2022-08-26 10:46:38 -0400

    edit dist/Deploy to dist/deploy in server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/Deploy-git'));
+app.use(express.static(__dirname + '/dist/deploy-git'));
 
 // Link index.html of build folder with router.
 app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/Deploy-git/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/deploy-git/index.html'));
 });
 
 
